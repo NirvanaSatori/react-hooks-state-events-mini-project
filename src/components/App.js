c7c3889483de12a73945c5bf446d54a97498788b
MORRIS <morris24kariuki@gmail.com>
2023-01-24 18:47:36 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,44 @@
-import React from "react";
+import React , { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
 
 import { CATEGORIES, TASKS } from "../data";
+
 console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+
+  const [tasks, setTasks] = useState(TASKS);
+  const [category, setCategory] = useState("All");
+
+  function handleAddTask(newTask) {
+    setTasks([...TASKS, newTask])
+  }
+
+  function handleDeleteTask(deletedTask) {
+    setTasks(tasks.filter((task) => task.text !== deletedTask))
+
+  }
+  const visibleTasks = tasks.filter(
+    (task) => category === "All" || task.category === category
+  )
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter 
+         categories={CATEGORIES}
+         selectedCategory={category}
+         onSelectCategory={setCategory}/>
+      <NewTaskForm 
+          onTaskFormSubmit={handleAddTask} 
+          // categories={CATEGORIES}
+          categories={CATEGORIES.filter((category) => category !== "All")}/>
+      <TaskList
+          onDeleteTask={handleDeleteTask} 
+          visibleTasks={visibleTasks} />
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,18 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({ visibleTasks , onDeleteTask }) {
+  console.log(visibleTasks)
+
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
-    </div>
-  );
+    {
+      visibleTasks.map((task) => 
+      <Task key={task.text} text={task.text} 
+      category={task.category} onDeleteTask={onDeleteTask}/>)
+    }
+    </div>)
 }
+  
 
 export default TaskList;
